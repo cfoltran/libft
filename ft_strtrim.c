053a4e36a54e32253c0a13d1eae5a283da2d97da
Clément FOLTRAN <clementfoltran@mbp-de-clement.home>
2018-11-12 22:33:55 +0100

    fix ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,54 +6,31 @@
 /*   By: clfoltra <clfoltra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 17:51:05 by clfoltra          #+#    #+#             */
-/*   Updated: 2018/11/12 21:01:35 by clfoltra         ###   ########.fr       */
+/*   Updated: 2018/11/12 22:30:47 by clfoltra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-int		is_blank(char c)
-{
-	if (c == ' ' || c == '\t' || c == '\n')
-		return (1);
-	return (0);
-}
-
-int		str_len(const char *s)
-{
-	int		i;
-	int		len;
-
-	len = 0;
-	i = 0;
-	while (s[i])
-	{
-		if (!is_blank(s[i]))
-			len++;
-		i++;
-	}
-	return (len);
-}
 
 char	*ft_strtrim(char const *s)
 {
 	char	*res;
 	int		len;
 	int		i;
-	int		j;
 
-	j = 0;
-	i = 0;
-	len = str_len(s);
-	if ((res = (char*)malloc(sizeof(char) * len + 1)) == NULL)
+	len = ft_strlen(s);
+	while (s[len - 1] == ' ' || s[len - 1] == '\t' || s[len - 1] == '\n')
+		len--;
+	i = -1;
+	while (s[++i] == ' ' || s[i] == '\t' || s[i] == '\n')
+		len--;
+	len = (len < 0) ? 0 : len;
+	if (!(res = (char*)malloc(sizeof(res) * (len + 1))))
 		return (NULL);
-	while (s[i])
-	{
-		if (is_blank(s[i]))
-			i++;
-		while (s[i] && !is_blank(s[i]))
-			res[j++] = s[i++];
-	}
-	res[j] = 0;
+	s = s + i;
+	i = -1;
+	while (++i < len)
+		res[i] = *s++;
+	res[i] = '\0';
 	return (res);
 }
